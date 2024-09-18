44c218bcfe1cdddab83b6f1358be8d335aeb2129
akashTharuka <akash_tharuka@yahoo.com>
2024-09-18 18:10:18 +0530

    service method to get all events

--- src/main/java/com/cml/assignment/controller/EventController.java
+++ src/main/java/com/cml/assignment/controller/EventController.java
@@ -1,13 +1,16 @@
 package com.cml.assignment.controller;
 
+import com.cml.assignment.model.Event;
 import com.cml.assignment.service.EventService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/event")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class EventController {
 
@@ -16,5 +19,10 @@ public class EventController {
     @GetMapping("/")
     public String greet() {
         return eventService.greet();
+    }
+
+    @GetMapping("/events")
+    public List<Event> getEvents() {
+        return eventService.getEvents();
     }
 }
--- src/main/java/com/cml/assignment/service/EventService.java
+++ src/main/java/com/cml/assignment/service/EventService.java
@@ -1,12 +1,24 @@
 package com.cml.assignment.service;
 
+import com.cml.assignment.model.Event;
+import com.cml.assignment.repo.EventRepo;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class EventService {
+
+    private final EventRepo eventRepo;
+
     public String greet() {
         return "Hello World";
     }
 
 
+    public List<Event> getEvents() {
+        return eventRepo.findAll();
+    }
 }
